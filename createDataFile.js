5f8143b5df5f46c3a0707e29097d395b4799c5f9
lee-philip-n <lee.philip.n@gmail.com>
2018-03-15 11:13:42 -0700

    refactored createDataFile.js

--- createDataFile.js
+++ createDataFile.js
@@ -1,35 +1,39 @@
 const fs = require('fs');
 const faker = require('faker');
 
-const write = (start, end) => {
-  let container = [];
-  let i = start;
-  while (true) {
-    const data = {
-      id: i,
-      name: faker.Company.companyName() + faker.Name.lastName(),
-      about: {
-        description: faker.Lorem.sentences(),
-        hours: faker.Date.recent(),
-        price: faker.Helpers.randomNumber(100),
-        style: faker.Lorem.words(),
-        phone: faker.PhoneNumber.phoneNumber(),
-      },
-      banner: [faker.Image.nightlife(), faker.Image.nightlife(), faker.Image.nightlife(), faker.Image.nightlife()],
-      photo: [faker.Image.food(), faker.Image.food(), faker.Image.food()],
-    };
-    container.push(data);
-    i++;
-    if (i !== 0 && i % 500000 === 0) {
-      container.map(obj => {
-        fs.appendFileSync(`./sampleDatas/sampleData${i}.json`, JSON.stringify(obj)+'\n');
-      });
-      container = [];
-    }
-    if (i === end) {
-      break;
-    }
-  }
-};
+const writeStream = fs.createWriteStream('sampleDatas/data.json');
 
+function createDataFile(start, end, stream, encoding, callback) {
+  let i = start;
+  const write = () => {
+    let flag = true;
+    while (i > end && flag) {
+      const data = {
+        id: i,
+        name: faker.company.companyName() + faker.name.lastName(),
+        about: {
+          description: faker.lorem.sentences(),
+          hours: faker.date.weekday(),
+          price: faker.random.number(100),
+          style: faker.lorem.word(),
+          phone: faker.phone.phoneNumber(),
+        },
+        banner: [faker.image.city(), faker.image.nature(), faker.image.business(), faker.image.nightlife()],
+        photo: [faker.image.food(), faker.image.animals(), faker.image.cats()],
+      };
+      i -= 1;
+      if (i === end) {
+        stream.write(JSON.stringify(data)+'\n', encoding, callback);
+      } else {
+        flag = stream.write(JSON.stringify(data)+'\n', encoding);
+      }
+    }
+    if (i > end) {
+      stream.once('drain', write);
+    }
+  };
+  write();
+}
+
+// createDataFile(20000000, 10000000, writeStream, 'utf8', () => console.log('done'));
 //mongoimport --db restaurant --collection abouts --file sampleDatas/sampleData.json
--- db/database.js
+++ db/database.js
@@ -1,75 +1,56 @@
-// const faker = require('faker');
-const mongoose = require('mongoose');
-// mongoose.Promise = require('bluebird');
+/*
+MONGODB
+*/
+// const mongoose = require('mongoose');
 
-const connection = 'mongodb://localhost/restaurant' || `mongodb://${process.env.DB_USER}:${process.env.DB_PW}@ds259778.mlab.com:59778/abouts`;
+// const connection = 'mongodb://localhost/restaurant' || `mongodb://${process.env.DB_USER}:${process.env.DB_PW}@ds259778.mlab.com:59778/abouts`;
 
-console.log('connection', connection);
-mongoose.connect(connection);
+// console.log('connection', connection);
+// mongoose.connect(connection);
 
-const aboutSchema = mongoose.Schema({
-  id: {
-    type: Number,
-    unique: true,
-  },
-  name: String,
-  about: {
-    description: String,
-    hours: Number,
-    price: Number,
-    style: String,
-    phone: String,
-  },
-  banner: [],
-  photo: [],
-});
+// const aboutSchema = mongoose.Schema({
+//   id: {
+//     type: Number,
+//     unique: true,
+//   },
+//   name: String,
+//   about: {
+//     description: String,
+//     hours: String,
+//     price: Number,
+//     style: String,
+//     phone: String,
+//   },
+//   banner: [],
+//   photo: [],
+// });
 
+// const About = mongoose.model('About', aboutSchema);
 
-const About = mongoose.model('About', aboutSchema);
-
-// const insert = (i, max) => {
-//   if (i === max) {
-//     mongoose.disconnect();
-//   }
-//   const data = {
-//     id: i,
-//     name: faker.Company.companyName() + faker.Name.lastName(),
-//     about: {
-//       description: faker.Lorem.sentences(),
-//       hours: faker.Date.recent(),
-//       price: faker.Helpers.randomNumber(100),
-//       style: faker.Lorem.words(),
-//       phone: faker.PhoneNumber.phoneNumber(),
-//     },
-//     banner: [faker.Image.nightlife(), faker.Image.nightlife(), faker.Image.nightlife(), faker.Image.nightlife()],
-//     photo: [faker.Image.food(), faker.Image.food(), faker.Image.food()],
-//   };
-
-//   About.create(data).then(() => {
-//     insert(i+1, max);
-//   }).catch(err => console.error(err));
+// const find = (obj, cb) => {
+//   About.find(obj, (err, about) => {
+//     if (err) {
+//       cb(err, null);
+//     } else {
+//       cb(null, about);
+//     }
+//   });
 // };
 
-const find = (obj, cb) => {
-  About.find(obj, (err, about) => {
-    if (err) {
-      cb(err, null);
-    } else {
-      cb(null, about);
-    }
-  });
-};
+// const findOne = (obj, cb) => {
+//   About.findOne({}, (err, results) => {
+//     if (err) {
+//       cb(err, null);
+//     } else {
+//       cb(null, results);
+//     }
+//   });
+// };
 
-const findOne = (obj, cb) => {
-  About.findOne({}, (err, results) => {
-    if (err) {
-      cb(err, null);
-    } else {
-      cb(null, results);
-    }
-  });
-};
+// module.exports.find = find;
+// module.exports.findOne = findOne;
 
-module.exports.find = find;
-module.exports.findOne = findOne;
+/*
+POSTGRESQL
+*/
 
